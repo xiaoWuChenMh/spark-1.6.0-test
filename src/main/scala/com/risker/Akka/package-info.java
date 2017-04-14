ea60fca43b7c2c13fad4737c983a6b8e231ea304
mh <944654085@qq.com>
2017-04-14 16:52:58 +0800

    说明

---
+++ src/main/scala/com/risker/Akka/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Akka并发编程模型——Actor
+ * 通过对阿里云溪（https://yq.aliyun.com/topic/69）的关于Akka的学习
+ * 做出的代码和笔记，相识度90%,只是在语言上做了一些自己的改动
+ * 笔记：
+ *  http://note.youdao.com/noteshare?id=9f946f2b8735f06501f2c8a829d87168
+ *
+ * Created by hc-3450 on 2017/4/14.
+ */
+package com.risker.Akka;
